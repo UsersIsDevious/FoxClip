c4b1b0bf84a4509123a34e5b405f194ce17c08b0
Nick-Sugar <nicksugar0141@gmail.com>
2025-08-15 21:48:20 +0900

    Refactor CMakeLists.txt and plugin-main.cpp for improved readability and formatting

--- src/app/plugin-main.cpp
+++ src/app/plugin-main.cpp
@@ -3,11 +3,9 @@
 #include <obs-module.h>
 #include <obs-frontend-api.h>
 
-
-#include "infra_shared/log/ObsLogger.h"  
+#include "infra_shared/log/ObsLogger.h"
 #include "infra_shared/support/plugin-support.h"
 #include "features/startup_check/app/StartupCheckFacade.h"
-
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
@@ -16,22 +14,21 @@ bool obs_module_load(void)
 {
 	// loding message
 	OBS_LOG_INFO("plugin loaded successfully (version %s)", PLUGIN_VERSION);
-	
+
 	//checking plugins directory
 	using foxclip::features::startup_check::app::StartupCheckFacade;
-    StartupCheckFacade facade(".", "foxclip-plugins");
-    auto r = facade.run();
+	StartupCheckFacade facade(".", "foxclip-plugins");
+	auto r = facade.run();
 
 	// Set Custom QAction in OBS Tools menu
-	QAction *act = (QAction*)obs_frontend_add_tools_menu_qaction("FoxClip Mmenu");
-    if (!act) {
-        OBS_LOG_ERROR("Failed to create Tools menu QAction");
-        return true;
-    }
-	QObject::connect(act, &QAction::triggered, [](){
-		OBS_LOG_INFO("QAction triggered (version %s)", PLUGIN_VERSION);
-    });
-	
+	QAction *act = (QAction *)obs_frontend_add_tools_menu_qaction("FoxClip Mmenu");
+	if (!act) {
+		OBS_LOG_ERROR("Failed to create Tools menu QAction");
+		return true;
+	}
+	QObject::connect(act, &QAction::triggered,
+			 []() { OBS_LOG_INFO("QAction triggered (version %s)", PLUGIN_VERSION); });
+
 	return true;
 }
 
